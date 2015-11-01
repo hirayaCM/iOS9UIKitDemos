39f60354e95226c9eef2a34b9b23b261f86e1083
Shingo Hiraya <shingohiraya@gmail.com>
2015-11-02 02:21:40 +0900

    Update SpacerViewController

--- iOS9UIKitDemos/SpacerViewController.m
+++ iOS9UIKitDemos/SpacerViewController.m
@@ -18,36 +18,62 @@
 {
     [super viewDidLoad];
     
-    UIView *leftView = [UIView new];
-    leftView.backgroundColor = [UIColor redColor];
-    leftView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:leftView];
+    UIButton *fooButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [fooButton setTitle:@"foo" forState:UIControlStateNormal];
+    fooButton.titleLabel.font = [UIFont systemFontOfSize:20.0f];
+    fooButton.layer.borderColor = fooButton.tintColor.CGColor;
+    fooButton.layer.borderWidth = 1.0f;
+    fooButton.layer.cornerRadius = 4.0f;
+    fooButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:fooButton];
     
-    UIView *rightView = [UIView new];
-    rightView.backgroundColor = [UIColor blueColor];
-    rightView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:rightView];
+    UIButton *barButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [barButton setTitle:@"bar" forState:UIControlStateNormal];
+    barButton.titleLabel.font = [UIFont systemFontOfSize:20.0f];
+    barButton.layer.borderColor = fooButton.tintColor.CGColor;
+    barButton.layer.borderWidth = 1.0f;
+    barButton.layer.cornerRadius = 4.0f;
+    barButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:barButton];
     
-    UILayoutGuide *spacer = [UILayoutGuide new];
-    [self.view addLayoutGuide:spacer];
+    UILayoutGuide *spacer1 = [UILayoutGuide new];
+    [self.view addLayoutGuide:spacer1];
     
-    // leftView
-    [leftView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
-    [leftView.trailingAnchor constraintEqualToAnchor:spacer.leadingAnchor].active = YES;
-    [leftView.widthAnchor constraintEqualToAnchor:rightView.widthAnchor].active = YES;
-    [leftView.heightAnchor constraintEqualToConstant:100.0f].active = YES;
-    [leftView.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+    UILayoutGuide *spacer2 = [UILayoutGuide new];
+    [self.view addLayoutGuide:spacer2];
     
-    // rightView
-    [rightView.leadingAnchor constraintEqualToAnchor:spacer.trailingAnchor].active = YES;
-    [rightView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
-    [rightView.topAnchor constraintEqualToAnchor:leftView.topAnchor].active = YES;
-    [rightView.heightAnchor constraintEqualToAnchor:spacer.heightAnchor].active = YES;
+    UILayoutGuide *spacer3 = [UILayoutGuide new];
+    [self.view addLayoutGuide:spacer3];
     
-    // spacer
-    [spacer.topAnchor constraintEqualToAnchor:leftView.topAnchor].active = YES;
-    [spacer.widthAnchor constraintEqualToConstant:50.0f].active = YES;
-    [spacer.heightAnchor constraintEqualToAnchor:leftView.heightAnchor].active = YES;
+    // fooButton
+    [fooButton.leadingAnchor constraintEqualToAnchor:spacer1.trailingAnchor].active = YES;
+    [fooButton.trailingAnchor constraintEqualToAnchor:spacer2.leadingAnchor].active = YES;
+    [fooButton.heightAnchor constraintEqualToConstant:44.0f].active = YES;
+    [fooButton.widthAnchor constraintEqualToAnchor:barButton.widthAnchor].active = YES;
+    [fooButton.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor].active = YES;
+    
+    // barButton
+    [barButton.leadingAnchor constraintEqualToAnchor:spacer2.trailingAnchor].active = YES;
+    [barButton.trailingAnchor constraintEqualToAnchor:spacer3.leadingAnchor].active = YES;
+    [barButton.heightAnchor constraintEqualToAnchor:fooButton.heightAnchor].active = YES;
+    [barButton.topAnchor constraintEqualToAnchor:fooButton.topAnchor].active = YES;
+    
+    // spacer1
+    [spacer1.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+    [spacer1.topAnchor constraintEqualToAnchor:fooButton.topAnchor].active = YES;
+    [spacer1.widthAnchor constraintEqualToConstant:50.0f].active = YES;
+    [spacer1.heightAnchor constraintEqualToAnchor:fooButton.heightAnchor].active = YES;
+    
+    // spacer2
+    [spacer2.topAnchor constraintEqualToAnchor:spacer1.topAnchor].active = YES;
+    [spacer2.widthAnchor constraintEqualToAnchor:spacer1.widthAnchor].active = YES;
+    [spacer2.heightAnchor constraintEqualToAnchor:spacer1.heightAnchor].active = YES;
+    
+    // spacer3
+    [spacer3.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+    [spacer3.topAnchor constraintEqualToAnchor:spacer1.topAnchor].active = YES;
+    [spacer3.widthAnchor constraintEqualToAnchor:spacer1.widthAnchor].active = YES;
+    [spacer3.heightAnchor constraintEqualToAnchor:spacer1.heightAnchor].active = YES;
 }
 
 - (void)didReceiveMemoryWarning

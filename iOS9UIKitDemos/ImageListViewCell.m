56f8f5331d79093c2cb7accf939417aa2c5f198a
Shingo Hiraya <shingohiraya@gmail.com>
2015-11-02 04:38:11 +0900

    Update ImageListViewCell

--- iOS9UIKitDemos/ImageListViewCell.m
+++ iOS9UIKitDemos/ImageListViewCell.m
@@ -12,24 +12,17 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
 
-@property (nonatomic) NSLayoutConstraint *heightConstraint;
-
 @end
 
 @implementation ImageListViewCell
 
 - (void)updateConstraints
 {
-    if (self.heightConstraint) {
-        self.heightConstraint.active = NO;
-    }
-
     CGFloat imageRatio = self.image.size.height / self.image.size.width;
     CGFloat imageViewHeight = floorf(self.bounds.size.width * imageRatio);
     
     // Change heightAnchor
-    self.heightConstraint = [self.photoImageView.heightAnchor constraintEqualToConstant:imageViewHeight];
-    self.heightConstraint.active = YES;
+    [self.photoImageView.heightAnchor constraintEqualToConstant:imageViewHeight].active = YES;
     
     [super updateConstraints];
 }
